e3ca1fa90bc36508b30ed39c8377906253f73b86
Kirstien <kirstien@Kirstiens-MacBook-Air.local>
2018-11-28 22:32:37 -0600

    Added content

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,23 @@ $(function() {
         placeHolder.append(html);
     });
 });
+
+$(function() {
+    var raw_template = $('#announcement-template').html();
+    var template = Handlebars.compile(raw_template);
+    var placeHolder = $("#announcement-list");
+    $.get("assets/data.json", function(datas, status, xhr) {
+        var html = template(datas);
+        placeHolder.append(html);
+    });
+});
+
+$(function() {
+    var raw_template = $('#faq-template').html();
+    var template = Handlebars.compile(raw_template);
+    var placeHolder = $("#faq-list");
+    $.get("assets/data.json", function(datas, status, xhr) {
+        var html = template(datas);
+        placeHolder.append(html);
+    });
+});
