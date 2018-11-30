c5510c883d5731a8327e650adc491457489fff42
Kirstien <kirstien@Kirstiens-MacBook-Air.local>
2018-11-29 18:43:16 -0600

    Updated file

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,38 @@
 $(function() {
-    var raw_template = $('#events-thursday-template').html();
+    var raw_template = $('#events-template').html();
     var template = Handlebars.compile(raw_template);
     var placeHolder = $("#events-thursday-list");
-    $.get("assets/data.json", function(datas, status, xhr) {
+    $.get("assets/data/events_thursday.json", function(datas, status, xhr) {
         var html = template(datas);
         placeHolder.append(html);
     });
 });
 
 $(function() {
-    var raw_template = $('#events-friday-template').html();
+    var raw_template = $('#events-template').html();
     var template = Handlebars.compile(raw_template);
     var placeHolder = $("#events-friday-list");
-    $.get("assets/data.json", function(datas, status, xhr) {
+    $.get("assets/data/events_friday.json", function(datas, status, xhr) {
         var html = template(datas);
         placeHolder.append(html);
     });
 });
 
 $(function() {
-    var raw_template = $('#events-saturday-template').html();
+    var raw_template = $('#events-template').html();
     var template = Handlebars.compile(raw_template);
     var placeHolder = $("#events-saturday-list");
-    $.get("assets/data.json", function(datas, status, xhr) {
+    $.get("assets/data/events_saturday.json", function(datas, status, xhr) {
         var html = template(datas);
         placeHolder.append(html);
     });
 });
 
 $(function() {
-    var raw_template = $('#events-sunday-template').html();
+    var raw_template = $('#events-template').html();
     var template = Handlebars.compile(raw_template);
     var placeHolder = $("#events-sunday-list");
-    $.get("assets/data.json", function(datas, status, xhr) {
+    $.get("assets/data/events_sunday.json", function(datas, status, xhr) {
         var html = template(datas);
         placeHolder.append(html);
     });
@@ -42,7 +42,7 @@ $(function() {
     var raw_template = $('#announcement-template').html();
     var template = Handlebars.compile(raw_template);
     var placeHolder = $("#announcement-list");
-    $.get("assets/data.json", function(datas, status, xhr) {
+    $.get("assets/data/announcements.json", function(datas, status, xhr) {
         var html = template(datas);
         placeHolder.append(html);
     });
@@ -52,7 +52,7 @@ $(function() {
     var raw_template = $('#sponsors-template').html();
     var template = Handlebars.compile(raw_template);
     var placeHolder = $("#sponsors-list");
-    $.get("assets/data.json", function(datas, status, xhr) {
+    $.get("assets/data/sponsors.json", function(datas, status, xhr) {
         var html = template(datas);
         placeHolder.append(html);
     });
@@ -62,13 +62,8 @@ $(function() {
     var raw_template = $('#faq-template').html();
     var template = Handlebars.compile(raw_template);
     var placeHolder = $("#faq-list");
-    $.get("assets/data.json", function(datas, status, xhr) {
+    $.get("assets/data/faq.json", function(datas, status, xhr) {
         var html = template(datas);
         placeHolder.append(html);
     });
-});
-
-
-Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
-    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
 });
